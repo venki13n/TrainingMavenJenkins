7eea31aaa4402c36207d25e93e3b5f2d5aa89f6d
Madaboina <s.madaboina@accenture.com>
2017-10-12 13:04:59 +0530

    first commit of maven project

---
+++ src/main/java/Test_Cigna/FirstTrainingMaven/App.java
@@ -0,0 +1,13 @@
+package Test_Cigna.FirstTrainingMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
